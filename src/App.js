8aebdef226a542b8620c7ae4c24515259bb2dd02
zoltan_almodo <zoltanalmodo@gmail.com>
2019-01-31 16:45:34 +0000

    purpose logo in the middle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/purpose-group-logo.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <nav className="App-nav">
+          <img src={logo} className="Purpose-logo" alt="logo" />
+        </nav>
       </div>
     );
   }
